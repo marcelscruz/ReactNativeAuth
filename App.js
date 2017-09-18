9d466db2b1d81890f21a693db9d3dad12e105a29
Marcel Cruz <marcelcruz@live.com>
2017-09-18 20:20:01 +0100

    log out button added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './src/components/common';
+import { Header, Button, Spinner, CardSection } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyC9j6apeRJt97xxr60xu0XZ2hAwOD7RwXs',
@@ -14,16 +16,49 @@ class App extends Component {
       storageBucket: 'auth-fc961.appspot.com',
       messagingSenderId: '122981550719'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button>Log Out</Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <View style={styles.viewStyle}>
+            <Spinner size='large' />
+          </View>
+        );
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
 }
+
+const styles = {
+  viewStyle: {
+    marginTop: 50
+  }
+};
 
 export default App;
