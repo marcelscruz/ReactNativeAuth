a389c9876a2236243f67b9e68ed301bee8215af5
Marcel Cruz <marcelcruz@live.com>
2017-09-18 14:26:56 +0100

    LoginForm and Input finished

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,33 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '' };
 
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }}
+          <Input
+            placeholder='user@email.com'
+            label='Email'
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            keyboardType='email-address'
+            autoCapitalize='none'
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry
+            placeholder='password'
+            label='Password'
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            autoCapitalize='none'
+          />
+        </CardSection>
 
         <CardSection>
           <Button>
