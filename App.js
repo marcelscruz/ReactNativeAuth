15edbb9ad58686b64336051a0ecf67b549cb4819
Marcel Cruz <marcelcruz@live.com>
2017-09-18 12:44:12 +0100

    firebase connected

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './src/components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyC9j6apeRJt97xxr60xu0XZ2hAwOD7RwXs',
+      authDomain: 'auth-fc961.firebaseapp.com',
+      databaseURL: 'https://auth-fc961.firebaseio.com',
+      projectId: 'auth-fc961',
+      storageBucket: 'auth-fc961.appspot.com',
+      messagingSenderId: '122981550719'
+    });
+  }
+
   render() {
     return (
       <View>
+        <Header headerText='Authentication' />
         <Text>An App!</Text>
         {console.log('test')}
       </View>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -13,21 +13,21 @@ const Header = (props) => {
 
 const styles = {
   viewStyle: {
-   backgroundColor: '#F8F8F8',
-   alignItems: 'center',
-   justifyContent: 'center',
-   height: 60,
-   paddingTop: 15,
-   shadowColor: '#000',
-   shadowOffset: { width: 0, height: 2 },
-   shadowOpacity: 0.2,
-   elevation: 2,
-   position: 'relative'
- },
+    backgroundColor: '#F8F8F8',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
 
- textStyle: {
-   fontSize: 20
- }
+  textStyle: {
+    fontSize: 20
+  }
 };
 
-export default Header;
+export { Header };
